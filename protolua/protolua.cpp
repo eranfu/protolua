fd9f0bf89387fdac858056ac0a6d72b7522164a4
eranfu <eranfu@tencent.com>
2022-04-28 16:02:33 +0800

    better decode

--- protolua/protolua.cpp
+++ protolua/protolua.cpp
@@ -84,10 +84,11 @@ static int decode(lua_State *L)
     if (!proto_decode(proto, L, data, size))
     {
         proto_error("proto.decode fail, proto=%s", proto);
-        lua_getglobal(L, "print");
-        lua_pushfstring(L, "proto.decode fail, proto=%s, size=%d", proto, size);
-        lua_call(L, 1, 0);
-        return 0;
+        lua_getglobal(L, "print"); // print
+        lua_pushfstring(L, "proto.decode fail, proto=%s, size=%d", proto, size); // print, error
+        lua_call(L, 1, 0); // empty
+        lua_pushnil(L); // nil
+        return 1;
     }
 
     return lua_gettop(L) - 2;
